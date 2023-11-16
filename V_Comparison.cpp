ea7730a0588f94af9665a6ecec5ccf5ae735d1ea
unknown <ijsathi2001rs@gmail.com>
2023-11-16 09:42:10 +0600

    V. Comparison

---
+++ V_Comparison.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    char s;
+    cin >> a >> s >> b;
+    if ((s == '<' && a < b) || (s == '>' && a > b) || (s == '=' && a == b))
+    {
+        cout << "Right";
+    }
+    else
+    {
+        cout << "Wrong";
+    }
+    return 0;
+}
